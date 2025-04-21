0bb10d8fd62ed8a178b2124b9691a7a8cd79f663
akhilgaddam55 <akhilkumargaddam5@gmail.com>
2025-04-21 13:04:38 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 9000;
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',  
-  port: process.env.DB_PORT || 5432, 
+  port: process.env.DB_PORT || 53749, 
 });
 
 
